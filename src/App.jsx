82b9d7b53a8e70da567d2083ce46958a6c43ab6b
佐藤佑樹 <s1932058jl@s.chibakoudai.jp>
2022-10-31 11:20:15 +0900

    読み取り精度向上のための実験 2

--- src/App.jsx
+++ src/App.jsx
@@ -64,6 +64,7 @@ const App = () => {
         frameRate: 10,
         facingMode: "environment"
       },
+      area: { top: "20%", right: "10%", left: "10%", bottom: "20%" },
       singleChannel: false
     },
     locator: {
@@ -194,7 +195,7 @@ const App = () => {
       )}
 
       {error && !loading && (
-        <div className="alert alert-warning shadow-lg justify-center absolute bottom-4 max-w-md mx-auto z-[99999]">
+        <div className="alert alert-warning shadow-lg justify-center absolute bottom-4 max-w-[400px] mx-auto z-[99999]">
           <div>
             <svg xmlns="http://www.w3.org/2000/svg" className="stroke-current flex-shrink-0 h-6 w-6" fill="none" viewBox="0 0 24 24"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z" /></svg>
             <pre>{error}</pre>
@@ -203,7 +204,7 @@ const App = () => {
       )}
 
       {success && !loading && (
-        <div className="alert alert-success shadow-lg justify-center absolute bottom-4 max-w-md mx-auto z-[99999]">
+        <div className="alert alert-success shadow-lg justify-center absolute bottom-4 max-w-[400px] mx-auto z-[99999]">
           <div>
             <svg xmlns="http://www.w3.org/2000/svg" className="stroke-current flex-shrink-0 h-6 w-6" fill="none" viewBox="0 0 24 24"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z" /></svg>
             <span>読み込みに成功しました</span>
